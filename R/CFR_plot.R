5e1a2523af80986118a40e364598188ce8e584aa
Zheng Chen <zheng-chen@hotmail.com>
2023-10-13 11:02:20 +0800

    Add files via upload

--- R/CFR_plot.R
+++ R/CFR_plot.R
@@ -1,7 +1,11 @@
 #' Plotting of CFR curves
-#' @usage  CFR_plot (CFR,time,xmin,xmax,ymax=1,lab.x="Study time",lab.y="CFR",col=ifelse(is.vector(CFR),1,c(1:ncol(CFR))),cex.lab=1.5, cex.axis=1.5,lwd=ifelse(is.vector(CFR),1,c(1:ncol(CFR))),lty=ifelse(is.vector(CFR),1,c(1:ncol(CFR))))
-#' @param CFR Vector or data frame, CFR values for different methods
+#' @usage  CFR_plot (data,method,time=data$day,m=NULL,sd=NULL,distr=NULL,xmin,xmax,ymax=1,lab.x="Study time",lab.y="CFR",col=c(1:length(method)),cex.lab=1.5, cex.axis=1.5,lwd=c(1:length(method)),lty=c(1:length(method)))
+#' @param data summarized survival data
+#' @param method Different methods of calculating the CFR. specifically "Yip","Chen","Garske","Nishiura","Ejima"
 #' @param time time Points of interest
+#' @param m Mean survival time of dead individuals. Methods "Garske", "Nishiura" and "Ejima"are required.
+#' @param sd Variance of survival time of dead individuals.Methods "Garske", "Nishiura" and "Ejima"are required.
+#' @param distr Distribution of survival time of dead individuals.Methods "Garske", "Nishiura" and "Ejima"are required.
 #' @param xmin Minimum value of the horizontal coordinate
 #' @param xmax Maximum value of horizontal coordinate
 #' @param ymax Maximum value of vertical coordinate
@@ -12,36 +16,46 @@
 #' @param cex.axis "cex.axis" parameters in function plot
 #' @param lwd "cex.axis" parameters in function plot
 #' @param lty "cex.axis" parameters in function plot
+#'
 #' @examples
-#' ###single method
-#' Yip_CFR<-c(0.198,0.048,0.531)
-#' time<-c(60,80,93)
-#' CFR_plot(Yip_CFR,time,60,100)
-#' ###Multiple methods
-#' CFRs<-data.frame(Chen=c(0.433,0.467,0.777),Yoshikura=c(0.157,0.149,0.122),Yip.cfr=c(0.198,0.048,0.531),Garske=c(0.246,0.252,0.298),Nishiura=c(0.308,0.312,0.318),Ejima=c(0.108,0.098,0.039))
-#' time<-c(60,80,93)
-#' CFR_plot(CFRs,time,60,100)
-CFR_plot<-function(CFR,time,xmin,xmax,ymax=1,lab.x="Study time",lab.y="CFR",col=if(is.vector(CFR))1 else c(1:ncol(CFR)),
-                   cex.lab=1.5, cex.axis=1.5,lwd=if(is.vector(CFR))1 else c(1:ncol(CFR)),lty=if(is.vector(CFR))1 else c(1:ncol(CFR))){
-  if (is.vector(CFR)){
-    plot(time,CFR,type="l",xlim=c(xmin,xmax),ylim=c(0,ymax),xlab=lab.x,ylab=lab.y,
-         cex.lab=cex.lab,cex.axis=cex.axis,lty=lty,lwd=lwd,col=col)
+#' data(individual)
+#' data1<-sumdata(individual)
+#' CFR_plot(data=datasum,method=c("Yip","Chen","Garske"),m=25.67,sd=71.74,distr=2,xmin=0,xmax=100)
+#' CFR_plot(data=datasum,method=c("Yip","Chen"),xmin=0,xmax=100)
+CFR_plot<-function(data,method,time=data$day,m=NULL,sd=NULL,distr=NULL,xmin,xmax,ymax=1,lab.x="Study time",lab.y="CFR",col=c(1:length(method)),
+                   cex.lab=1.5, cex.axis=1.5,lwd=c(1:length(method)),lty=c(1:length(method))){
+  n<-length(time)
+  Yip_method<-c()
+  Chen_method<-c()
+  Garske_method<-c()
+  Nishiura_method<-c()
+  Ejima_method<-c()
+  for (i in 1:n){
+    if ("Yip" %in% method )
+      Yip_method[i]<-Yip.cfr(data,time[i],B=10)$CFR
+    if ("Chen" %in% method )
+      Chen_method[i]<-Chen(data,time[i])$CFR
+    if("Garske" %in% method )
+      Garske_method[i]<-Garske(data,time[i],m,sd,distr)$CFR
+    if ("Nishiura" %in% method )
+      Nishiura_method[i]<-Nishiura(data,time[i],m,sd,distr)$CFR
+    if ("Ejima"%in% method)
+      Ejima_method[i]<-Ejima(data,time[i],m,sd,distr)$CFR
   }
+  CFR<-cbind(Yip_method,Chen_method,Garske_method,Nishiura_method,Ejima_method)
+  #colnames(CFR)<-c("Yip_method","Chen_method","Garske_method","Nishiura_method","Ejima_method")
+  CFR<-as.data.frame(CFR)
+  #CFR<-na.omit(CFR)
+  n1<-ncol(CFR)
+  if(n1==1){
+    plot(time,CFR,type="l",xlim=c(xmin,xmax),ylim=c(0,ymax),xlab=lab.x,ylab=lab.y,
+         cex.lab=cex.lab,cex.axis=cex.axis,lty=lty,lwd=lwd,col=col)}
   else{
-    n<-ncol(CFR)
-    if(n==1){
-      plot(time,CFR,type="l",xlim=c(xmin,xmax),ylim=c(0,ymax),xlab=lab.x,ylab=lab.y,
-           cex.lab=cex.lab,cex.axis=cex.axis,lty=lty,lwd=lwd,col=col)}
-    else{
-      plot(time,CFR[,1],type="l",xlim=c(xmin,xmax),ylim=c(0,ymax),xlab=lab.x,ylab=lab.y,
-           cex.lab=cex.lab,cex.axis=cex.axis,lty=lty[1],lwd=lwd[1],col=col[1])
-      for(i in 2:n){
-        col1=col[i]
-        lwd1=lwd[i]
-        lty1=lty[i]
-        lines(time,CFR[,i],col=col1,lwd=lwd1,lty=lty1)
-      }
-      legend("topright",legend=colnames(CFR),lwd=lwd,lty=lty,col=col,title="CFR method")
+    plot(time,CFR[,1],type="l",xlim=c(xmin,xmax),ylim=c(0,ymax),xlab=lab.x,ylab=lab.y,
+         cex.lab=cex.lab,cex.axis=cex.axis,lty=lty[1],lwd=lwd[1],col=col[1])
+    for(i in 2:n1){
+      lines(time,CFR[,i],col=col[i],lwd=lwd[i],lty=lty[i])
     }
+    legend("topright",legend=colnames(CFR),lwd=lwd,lty=lty,col=col,title="CFR method")
   }
 }
